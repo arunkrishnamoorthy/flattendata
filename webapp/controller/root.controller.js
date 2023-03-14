fd13b38909e50d11e3009a2ddc6a2d811733a97d
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2023-03-14 19:58:35 +0100

    Update and delete

--- webapp/controller/root.controller.js
+++ webapp/controller/root.controller.js
@@ -9,6 +9,7 @@ sap.ui.define(
     return Controller.extend("ns.exercise5.controller.root", {
       onInit: function () {
         this._mColumns = {};
+        this._mRows = {};
         this._mCellIdentifier = {};
         this.loadData().attachEventOnce("requestCompleted", () => {
           this.formatData();
@@ -16,6 +17,9 @@ sap.ui.define(
         var oFlatDataModel = new sap.ui.model.json.JSONModel();
         this.getView().setModel(oFlatDataModel, "flat");
         this._oRowTemplate = { lineNo: "", item: "", code: "" };
+
+        var oCodeModel = new sap.ui.model.json.JSONModel({ code: "" });
+        this.getView().setModel(oCodeModel, "code");
       },
 
       loadData: function () {
@@ -27,12 +31,16 @@ sap.ui.define(
       formatData: function () {
         debugger;
         var oData = this.getView().getModel().getData();
+        this.getView()
+          .getModel("code")
+          .setData({ code: JSON.stringify(oData, undefined, 4) });
         var aCells = oData.DecisionTable["Cell"];
         var aColumns = oData.DecisionTable["Column"];
         var aRows = oData.DecisionTable["Row"];
 
         // Construct row structure by flattening columns
         this._mapColumns(aColumns);
+        this._mapRows(aRows);
 
         $.each(aCells, (index, cell) => {
           // row
@@ -79,6 +87,14 @@ sap.ui.define(
         });
       },
 
+      _mapRows: function (aRows) {
+        $.each(aRows, (index, row) => {
+          if (!this._mRows[row.Id]) {
+            this._mRows[parseInt(row.Id)] = row;
+          }
+        });
+      },
+
       handleSaveChanges: function (oEvent) {
         var aFlattendData = this.getView().getModel("flat").getData().rows;
         var aColumnKeys = Object.keys(this._oRowTemplate);
@@ -105,7 +121,122 @@ sap.ui.define(
             return element;
           });
         });
+        var oData = this.getView().getModel().getData();
+        oData.DecisionTable["Cell"] = aCells;
+        this.getView()
+          .getModel("code")
+          .setData({ code: JSON.stringify(oData, undefined, 4) });
+      },
+
+      handleAddNewRow: function () {
         debugger;
+        // Get the last row number.
+        var aKeys = Object.keys(this._mRows);
+        var iNewRowIndex = Math.max(...aKeys) + 1;
+        var aColumnKeys = Object.keys(this._mColumns);
+
+        const lineItem = `{
+          "ColumnId": "${aColumnKeys[0]}",
+          "RowId": "${iNewRowIndex}",
+          "Expression": "",
+          "AST": [
+              {
+                  "Id": "1",
+                  "Root": true,
+                  "Placeholder": true,
+                  "Children": [
+                      {
+                          "Id": "2",
+                          "SequenceNumber": 1
+                      },
+                      {
+                          "Id": "3",
+                          "SequenceNumber": 2
+                      }
+                  ]
+              },
+              {
+                  "Id": "3",
+                  "Value": " ",
+                  "Output": {
+                      "DataObjectType": "E",
+                      "BusinessDataType": "N"
+                  }
+              },
+              {
+                  "Id": "2",
+                  "Placeholder": true
+              }
+          ]
+        }`;
+
+        const item = `{
+          "ColumnId": "${aColumnKeys[1]}",
+          "RowId": "${iNewRowIndex}",
+          "Expression": "",
+          "AST": [
+              {
+                  "Id": "1",
+                  "Root": true,
+                  "Value": " ",
+                  "Output": {
+                      "DataObjectType": "E",
+                      "BusinessDataType": "S"
+                  }
+              }
+          ]
+        }`;
+
+        const code = ` {
+          "ColumnId": "${aColumnKeys[2]}",
+          "RowId": "${iNewRowIndex}",
+          "Expression": "",
+          "AST": [
+              {
+                  "Id": "1",
+                  "Root": true,
+                  "Value": " ",
+                  "Output": {
+                      "DataObjectType": "E",
+                      "BusinessDataType": "N"
+                  }
+              }
+          ]
+        }`;
+
+        var aNewRow = [
+          JSON.parse(lineItem),
+          JSON.parse(item),
+          JSON.parse(code),
+        ];
+        var oData = this.getView().getModel().getData();
+        oData.DecisionTable.Cell = [...oData.DecisionTable.Cell, ...aNewRow];
+        this.getView().getModel().setData(oData);
+        this.formatData();
+
+        this.getView()
+          .getModel("code")
+          .setData({ code: JSON.stringify(oData, undefined, 4) });
+      },
+
+      handleDeleteRow: function () {
+        debugger;
+        var oSelectedData = this.getView()
+          .byId("idNodes")
+          .getSelectedItem()
+          .getBindingContext("flat")
+          .getObject();
+        const [row, column] = oSelectedData.identifier;
+        var oData = this.getView().getModel().getData();
+        oData.DecisionTable["Cell"] = oData.DecisionTable["Cell"].filter(
+          (cell) => cell.RowId !== row
+        );
+        this.getView().setModel(oData);
+        this.getView()
+          .getModel("code")
+          .setData({ code: JSON.stringify(oData, undefined, 4) });
+        this.getView().byId("idNodes").getBinding("items").refresh();
+        this.formatData();
       },
     });
   }
